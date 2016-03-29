d3f5e4ce104af37fe9a3ecf723e2c19c400b55a7
sporokh@softserveinc.com <sporokh@softserveinc.com>
2016-03-29 15:32:17 +0300

    Added grafana dashboards

--- env-grafana/create-entities.sh
+++ env-grafana/create-entities.sh
@@ -8,16 +8,16 @@ done
 
 sed -i -e "s/_INFLUX_/$INFLUXDB_PORT_8086_TCP_ADDR:$INFLUXDB_PORT_8086_TCP_PORT/g" "/tmp/influxdb-datasource.json"
 
-# add datasources to grafana 
+# add datasources to grafana
 for item in $(ls /tmp/*-datasource.json); do
   curl -k -u $CREDENTIALS -H 'content-type: application/json' http://$HOST:$PORT/api/datasources -d@$item;
   echo "Datatosource created successfully $item"
   sleep 2
 done
 
-# add dashboards // DISABLED FOR NOW
-#for item in $(ls /tmp/*-dashboard.json); do
-#  curl -k -u $CREDENTIALS -H 'content-type: application/json' http://$HOST:$PORT/api/dashboards/db -d@$item;
-#  echo "Dashboard created successfully $item"
-#  sleep 2
-#done
+# add dashboards
+for item in $(ls /tmp/*-dashboard.json); do
+  curl -k -u $CREDENTIALS -H 'content-type: application/json' http://$HOST:$PORT/api/dashboards/db -d@$item;
+  echo "Dashboard created successfully $item"
+  sleep 2
+done
